1f320bc9237c4615ac57babac5dea6bfd9109049
Nik Field <nik.field@gmail.com>
2020-02-20 00:56:41 -0500

    Create download.php

---
+++ download.php
@@ -0,0 +1,15 @@
+<?php
+
+	define( 'DOING_AJAX', true );
+	require_once( '../../../wp-load.php' );
+	@header( 'Content-Type: text/html; charset=' . get_option( 'blog_charset' ) );
+	@header( 'X-Robots-Tag: noindex' );
+	send_nosniff_header();
+	header( 'Cache-Control: no-cache' );
+	header( 'Pragma: no-cache' );
+
+	$action = ( isset( $_POST['action'] ) ) ? $_POST['action'] : 'asset_download';
+
+	do_action( 'wp_ajax_' . $action );
+
+?>
