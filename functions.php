76ea8bc701c436822c1fc89ea13bc674f04f2642
Nik Field <nik.field@gmail.com>
2020-03-15 11:15:23 -0400

    fix login

--- functions.php
+++ functions.php
@@ -216,6 +216,17 @@
 
 	add_filter( 'big_image_size_threshold', '__return_false' );
 
+	/**
+	 * Set the URL to redirect to on login.
+	 *
+	 * @return string URL to redirect to on login. Must be absolute.
+	 */
+	function my_forcelogin_redirect() {
+		return home_url( '/' );
+	}
+
+	add_filter( 'v_forcelogin_redirect', 'my_forcelogin_redirect' );
+
 
 
 
