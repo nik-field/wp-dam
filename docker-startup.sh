0f56e7b53a4883161d321f43afe7a12dddbdade6
Nik Field <nik@ordinaryartistservices.com>
2020-06-10 16:43:45 -0400

    now using secrets!

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
-
-openssl rand -base64 20 | docker secret create mysql_password
-openssl rand -base64 20 | docker secret create mysql_root_password
+#!/bin/zsh
+mkdir -p $(cd ../ && pwd)/secrets/ && touch $(cd ../ && pwd)/secrets/mysql_password.secret
+mkdir -p $(cd ../ && pwd)/secrets/ && touch $(cd ../ && pwd)/secrets/mysql_root_password.secret
+openssl rand -out $(cd ../ && pwd)/secrets/mysql_password.secret -base64 20
+openssl rand -out $(cd ../ && pwd)/secrets/mysql_root_password.secret -base64 20
+docker-compose up -d --build
